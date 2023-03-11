aa58456e4739bb8fb9aaf3749e60e532587f0e73
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-03-11 08:35:38 +0100

    feat: idnow

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { IdnowManager } from '@tokenstreet/react-native-idnow-videoident';
 
 export default function App() {
+  const [transactionToken, setTransactionToken] = useState('YOUR_TRANSACTION_TOKEN');
+  const [videoIdentResponse, setVideoIdentResponse] = useState('');
+  const startVideoIdent = useCallback(() => {
+    setVideoIdentResponse('');
+    IdnowManager.startVideoIdent({transactionToken})
+        .then((fulfilled) => setVideoIdentResponse(`Resolved:\n${JSON.stringify(fulfilled)}`))
+        .catch((rejected) => setVideoIdentResponse(`Rejected:\n${JSON.stringify(rejected)}`));
+  }, [transactionToken]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput onChangeText={setTransactionToken} placeholder="Transaction token" value={transactionToken} />
+      <Button onPress={startVideoIdent} title="Start video ident" />
+      <Text>{videoIdentResponse}</Text>
       <StatusBar style="auto" />
     </View>
   );
